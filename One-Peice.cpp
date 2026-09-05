8c59d5271f733880bf0245b5c828aa28f7bbbf0a
Sub-Zero-Shubh <shubh2465@proton.me>
2026-09-05 14:44:17 +0530

    Add output redirection

--- One-Peice.cpp
+++ One-Peice.cpp
@@ -4,12 +4,28 @@
 #include<vector>
 #include<sstream>
 #include<cstring>
+#include<fcntl.h>
 using namespace std;
 int main(){
     string input;
 
     while(true){
-        cout<<"One-Peice>";
+        char cwd[1024];
+        char* current_dir=getcwd(cwd,sizeof(cwd));
+        char* home=getenv("HOME");
+
+        if(current_dir==nullptr){
+            perror("getcwd");
+            return 1;
+        }
+        string current=cwd;
+        if(home!=nullptr){
+        string home_dir=home;
+        if(current.find(home_dir)==0){
+            current.replace(0,home_dir.size(),"~");
+        }
+        }
+        cout<<"Enma:"<<current<<">";
         getline(cin,input);
         if(input=="exit")break;
         //Split input into tokens (Break the input string into words to use it as commands)
@@ -28,6 +44,7 @@ int main(){
                 if(tokens.size()<2){
                     cerr<<"cd: missing arguments \n";
                 }
+
                 else{
                 //chdir(const char*path) change current working directory using path. Returns 0 success and 1 on failure
                 //c_str() c++ to c style string
@@ -38,6 +55,50 @@ int main(){
                 continue;
 
             }
+            if(tokens[0]=="pwd"){
+                char pwd[1024];
+                char* present_dir=getcwd(pwd,sizeof(pwd));
+                if(present_dir==nullptr){
+                    perror("pwd");
+                }
+                else{
+                cout<<present_dir<<endl;
+                }
+                continue;
+            }
+
+            if(tokens[0]=="help"){
+                cout<<"Built-in commands:"<<endl;
+                cout<<"cd <directory> - Change current directory to <directory>"<<endl;
+                cout<<"pwd - Get present workind directory"<<endl;
+                cout<<"exit- Exit from current terminal session"<<endl;
+                continue;
+            }
+                      bool IO=false;
+                      bool valid=true;
+
+            string pathname;
+              for(int i=0;i<tokens.size();i++){
+                if(tokens[i]==">"){
+                    //redirection
+                    if(i+1<tokens.size()){
+                    pathname=tokens[i+1];
+                    IO=true;
+                     tokens.erase(tokens.begin()+i,tokens.begin()+i+2);
+
+                    }
+                    else{
+                        cerr<<"syntax error: expected filename after '>' "<<endl;
+                        valid=false;
+                    }
+                }
+
+
+                }
+
+              if(!valid)continue;
+            
+
 
 
         //converts vector of string into char*
@@ -55,6 +116,27 @@ int main(){
         
         if(pid==0){
             //child
+            if(IO){
+                 int fd=open(pathname.c_str(),O_TRUNC | O_WRONLY | O_CREAT , 0644);
+
+                    if(fd==-1){
+                        perror("open");
+                        exit(1);
+                    
+                    }
+                    else{
+                        int newfd=dup2(fd,STDOUT_FILENO);
+                        if(newfd==-1){
+                            perror("dup2");
+                            exit(1);
+                        }
+                        if(close(fd)==-1){
+                            perror("close");
+                            exit(1);
+                        }
+                    }
+            }
+
             //args[0]is command which to execute and args.data is what arguments to pass to that program
             //like execvp("ls",{"ls","-l",nullptr});
             execvp(args[0],args.data());
